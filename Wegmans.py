25b9dad590a4a20823eeca73bc9edcf932900207
nicole.ganung@outlook.com <iisixstIISIXST97>
2018-10-24 17:22:25 -0400

    started defining functions

--- Wegmans.py
+++ Wegmans.py
@@ -1,15 +1,25 @@
 # CSCI320 - Toys4Us
 # Authors:
-# Nicole Ganung 
+# Nicole Ganung
 # Wegmans UI Database
 
-empOrCust = raw_input("Are you an employee or customer?\n")
+def shoppersClubCustomer():
+	shoppersClubId = raw_input("Please enter your shoppersClubId")
+	shoppersClubPurchase(shoppersClubId)
+
+def shoppersClubPurchase(shoppersClubName):
+	numItems = raw_input("How many different items are you buying today?")
+
+def itemPurchase():
+	numItems = raw_input("How many different items are you buying today?")
+
+empOrCust = input('Are you an employee or customer?\n')
 if empOrCust.lower() == "employee":
-	print("hello")
+        print("hello employee")
 if empOrCust.lower() == "customer":
-	shoppersClub = raw_input("Are you a shoppers club member?")
-	shoppersClubCustomer(shoppersClub)
-	if (shoppersClub.lower() == "yes") | (shoppersClub.lower() == "y"):
-		numItems = raw_input("How many different items are you buying today?)
-		
-def 
+        shoppersClub = raw_input("Are you a shoppers club member?")
+        if (shoppersClub.lower() == "yes") | (shoppersClub.lower() == "y"):
+        	shoppersClubCustomer()
+        elif (shoppersClub.lower() == "no") | (shoppersClub.lower() == "n"):
+        	itemPurchase()
+        	# Do you want to sign up for a shoppers club card today?
