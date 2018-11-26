73ed133208bcccb790498c8e76c571c7859ef841
elanaaronson <era9862@rit.edu>
2018-11-26 16:57:18 -0500

    Finished vendor

--- Wegmans.py
+++ Wegmans.py
@@ -10,139 +10,165 @@ conn = psycopg2.connect(host="reddwarf.cs.rit.edu", user="p32002h", password="ei
 cursor = conn.cursor();
 
 def shoppersClubCustomer():
-	shoppersClubId = input("Please enter your shoppersClubId")
-	shoppersClubPurchase(shoppersClubId)
+    shoppersClubId = input("Please enter your shoppersClubId")
+    shoppersClubPurchase(shoppersClubId)
 
 
 def shoppersClubPurchase(shoppersClubName):
-	numItems = input("How many different items are you buying today?")
+    numItems = input("How many different items are you buying today?")
 
 
 def itemPurchase():
-	numItems = input("How many different items are you buying today?")
+    numItems = input("How many different items are you buying today?")
 
 def employee():
-	employeeId = int(input("Please enter your ID Number: "))
-	# search for ID number
-	employeeExists = False
-	cursor.execute('SELECT employee_id FROM employee WHERE employee_id = %s', (employeeId,))
-	# fetchall() gives back a array of inputs 
-	# fetchone() gives back one element 
-	myresult = cursor.fetchall()
-	for data in myresult:
-		if data[0] == employeeId:
-			employeeExists = True
+    employeeId = int(input("Please enter your ID Number: "))
+    # search for ID number
+    employeeExists = False
+    cursor.execute('SELECT employee_id FROM employee WHERE employee_id = %s', (employeeId,))
+    myresult = cursor.fetchall()
+    for data in myresult:
+        if data[0] == employeeId:
+            employeeExists = True
 
-	while not employeeExists:
-		print("You are not an employee in our system, please try again")
-		employeeId = int(input("Please enter your ID Number: "))
-		cursor.execute('SELECT employee_id FROM employee WHERE employee_id = %s', (employeeId,))
-		myresult = cursor.fetchall();
-		for data in myresult:
-			if data[0] == employeeId:
-				employeeExists = True
-			break
+    while not employeeExists:
+        print("You are not an employee in our system, please try again")
+        employeeId = int(input("Please enter your ID Number: "))
+        cursor.execute('SELECT employee_id FROM employee WHERE employee_id = %s', (employeeId,))
+        myresult = cursor.fetchall();
+        for data in myresult:
+            if data[0] == employeeId:
+                employeeExists = True
+            break
 
-	while True:
-		print("Thank you for logging into the system. Here is what you can do.")
-		print("0. Exit")
-		print("1. Look at 20 most recent items purchased")
-		print("2. Look at items with low inventory")
-		print("3. Order inventory")
-		choice = int(input("Please enter the number of the action you'd like to take:"))
-		if choice == 0:
-			print("Logging out...")
-			break
-		elif choice == 1:
-			#database operations
-			counter = 0
-			cursor.execute('SELECT * FROM receipt')
-			receiptResult = cursor.fetchall()
-			for data in receiptResult:
-				print(data[counter], " units of ", data[counter+1], " purchased for", data[counter+2], " on ", data[counter+3])
-			print("x units of y purchased for $z on [Date]")
-			print("x units of y purchased for $z on [Date]")
-			print("x units of y purchased for $z on [Date]")
-			print(".....")
-		elif choice == 2:
-			#database operations
-			counter = 1 
-			# cursor.execute('SELECT * FROM inventory')
-			print("Items with low inventory are: ")
-			print("Coke: 25 units")
-			print("Bread: 10 units")
-		elif choice == 3:
-			itemName = input("Enter the name of the item you would like to order: ")
-			itemAmount = int(input("Please enter the number of this item you would like to order: "))
-			#database operations
-			#cursor.execute('SELECT * FROM inventory')
-			print(str(itemAmount) + " units of " + itemName + " ordered")
-		else:
-			print("Incorrect choice. Please try again")
+    while True:
+        print("Thank you for logging into the system. Here is what you can do.")
+        print("0. Exit")
+        print("1. Look at 20 most recent items purchased")
+        print("2. Look at items with low inventory")
+        print("3. Order inventory")
+        choice = int(input("Please enter the number of the action you'd like to take:"))
+        if choice == 0:
+            print("Logging out...")
+            break
+        elif choice == 1:
+            #database operations
+            print("x units of y purchased for $z on [Date]")
+            print("x units of y purchased for $z on [Date]")
+            print("x units of y purchased for $z on [Date]")
+            print(".....")
+        elif choice == 2:
+            #database operations
+            print("Items with low inventory are: ")
+            print("Coke: 25 units")
+            print("Bread: 10 units")
+        elif choice == 3:
+            itemName = input("Enter the name of the item you would like to order: ")
+            itemAmount = int(input("Please enter the number of this item you would like to order: "))
+            #database operations
+            print(str(itemAmount) + " units of " + itemName + " ordered")
+        else:
+            print("Incorrect choice. Please try again")
 
 def vendor():
-	id = int(input("Please enter your ID Number: "))
-	# If it doesn't exist:
-	# Would you like to create a account?
-	while True:
-		print("Thank you for logging into the system. Here is what you can do:")
-		print("0. Exit")
-		print("1. Check inventory")
-		print("2. Add to inventory")
-		choice = int(input("Please enter the number of the action you'd like to take:"))
-		if choice == 0:
-			print("Logging out...")
-			break
-		elif choice == 1:
-			#database operations
-			print("Coke: 345 units")
-			print("Bread: 479 units")
-		elif choice == 2:
-			itemName = input("Enter the name of the item you would like to order: ")
-			itemAmount = int(input("Please enter the number of this item you would like to order: "))
-			# database operations
-			print(str(itemAmount) + " units of " + itemName + " ordered")
-		else:
-			print("Incorrect choice. Please try again")
+    vendorId = int(input("Please enter your ID Number: "))
+    vendorExists = False
+    cursor.execute('SELECT vendor_id FROM vendor WHERE vendor_id = %s', (vendorId,))
+    myresult = cursor.fetchall()
+    for data in myresult:
+        if data[0] == vendorId:
+            vendorExists = True
+
+    while not vendorExists:
+        print("You are not a vendor in our system, please try again")
+        vendorId = int(input("Please enter your ID Number: "))
+        cursor.execute('SELECT vendor_id FROM vendor WHERE vendor_id = %s', (vendorId,))
+        myresult = cursor.fetchall();
+        for data in myresult:
+            if data[0] == vendorId:
+                vendorExists = True
+                break
+
+    while True:
+        print("Thank you for logging into the system. Here is what you can do:")
+        print("0. Exit")
+        print("1. Check inventory")
+        print("2. Add to inventory")
+        choice = int(input("Please enter the number of the action you'd like to take:"))
+
+        if choice == 0:
+            print("Logging out...")
+            break
+        elif choice == 1:
+            # database operations
+            cursor.execute('SELECT * FROM inventory_item as inv natural left outer join product WHERE inv.inventory_id in (select inventory_id from vendor where vendor_id = %s)', (vendorId,))
+            result = cursor.fetchall()
+            for data in result:
+                print(data[6] + ": " + str(data[3]) + " units")
+
+            print("Coke: 345 units")
+            print("Bread: 479 units")
+        elif choice == 2:
+            itemName = input("Enter the name of the item you would like to order: ")
+            itemAmount = int(input("Please enter the number of this item you would like to order: "))
+            # database operations
+            cursor.execute("Select upc from product where name = '"+itemName+"'")
+            result = cursor.fetchone()
+            if len(result) == 0:
+                print("Not a valid product. Please try again")
+                break
+            itemUPC = result[0]
+            cursor.execute("Select * from inventory_item where upc = "+ str(itemUPC) +" and inventory_id in (select inventory_id from vendor where vendor_id = %s)", (vendorId,))
+            result = cursor.fetchone()
+            if len(result) == 0:
+                print("Item not in inventory. Please try again")
+                break
+            cursor.execute("update inventory_item set amount = "+ str(int(result[3]) + itemAmount) + " where upc = " + str(itemUPC))
+            print(str(itemAmount) + " units of " + itemName + " ordered")
+            cursor.execute("Select * from inventory_item where upc = " + str(
+            itemUPC) + " and inventory_id in (select inventory_id from vendor where vendor_id = %s)", (vendorId,))
+            result = cursor.fetchone()
+        else:
+            print("Incorrect choice. Please try again")
 
 def customer():
-	id = int(input("Please enter your ID Number: "))
-	# database operation 
-	# verify ID exists 
-	while True:
-		print("Thank you for logging into the system. Here is what you can do:")
-		print("0. Exit")
-		print("1. Make a purchase")
-		print("2. Get store info")
-		choice = int(input("Please enter the number of the action you'd like to take:"))
-		if choice == 0:
-			print("Logging out...")
-			break
-		elif choice == 1:
-			while True:
-				itemName = input("Enter the name of the item you would like to purchase or type exit: ")
-				if itemName.lower() == "exit":
-					#database operations
-					print("Your total is")
-					break
-				itemAmount = int(input("Please enter the number of this item you would like to purchase: "))
-		elif choice == 2:
-			#database operations
-			print("Our store hours are:")
-			print("MWF 11-5pm")
-			print("Address: 1 Lomb Memorial Dr.")
-		else:
-			print("Incorrect choice. Please try again")
+    id = int(input("Please enter your ID Number: "))
+    # database operation
+    # verify ID exists
+    while True:
+        print("Thank you for logging into the system. Here is what you can do:")
+        print("0. Exit")
+        print("1. Make a purchase")
+        print("2. Get store info")
+        choice = int(input("Please enter the number of the action you'd like to take:"))
+        if choice == 0:
+            print("Logging out...")
+            break
+        elif choice == 1:
+            while True:
+                itemName = input("Enter the name of the item you would like to purchase or type exit: ")
+                if itemName.lower() == "exit":
+                    #database operations
+                    print("Your total is")
+                    break
+                itemAmount = int(input("Please enter the number of this item you would like to purchase: "))
+        elif choice == 2:
+            #database operations
+            print("Our store hours are:")
+            print("MWF 11-5pm")
+            print("Address: 1 Lomb Memorial Dr.")
+        else:
+            print("Incorrect choice. Please try again")
 
 
 def main():
-	empOrCust = input('Please enter if you are an employee, vendor, or customer: ')
-	if empOrCust.lower() == "employee":
-		employee()
-	elif empOrCust.lower() == "customer":
-		customer()
-	elif empOrCust.lower() == "vendor":
-		vendor()
+    empOrCust = input('Please enter if you are an employee, vendor, or customer: ')
+    if empOrCust.lower() == "employee":
+        employee()
+    elif empOrCust.lower() == "customer":
+        customer()
+    elif empOrCust.lower() == "vendor":
+        vendor()
 
 if __name__ == '__main__':
     main()
